3cdad1a55018f0aa7a8f23474502e1208f02cbc1
jiyoshansj-art <jiyoshansj@gmail.com>
2026-09-09 22:55:31 +0530

    Update API client

--- webtourguide-web/src/api/axiosClient.js
+++ webtourguide-web/src/api/axiosClient.js
@@ -1,17 +1,24 @@
 import axios from "axios";
 
+const baseURL = "http://localhost:8080/api";
+
 const axiosClient = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL,
 });
 
-axiosClient.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-
-  return config;
-});
+axiosClient.interceptors.request.use(
+  (config) => {
+    try {
+      const token = localStorage.getItem("token");
+      if (token && token !== "undefined" && token !== "null") {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+    } catch (e) {
+      console.warn("Could not read token from localStorage:", e);
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default axiosClient;
