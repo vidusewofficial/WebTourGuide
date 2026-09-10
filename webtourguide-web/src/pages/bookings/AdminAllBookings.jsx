2e5517d09e8a633427e05ccebccdefb983e4514d
MedawalaRMWD <medawalarmwd@gmail.com>
2026-09-10 23:58:19 +0530

    Add state hook to AdminAllBookings

--- webtourguide-web/src/pages/bookings/AdminAllBookings.jsx
+++ webtourguide-web/src/pages/bookings/AdminAllBookings.jsx
@@ -2,4 +2,5 @@ import { useEffect, useState } from "react";
 import { getAllBookings } from "../../api/bookingApi";
 
 export default function AdminAllBookings() {
+  const [bookings, setBookings] = useState([]);
 }
