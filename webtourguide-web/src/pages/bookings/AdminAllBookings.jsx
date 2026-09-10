9afbc74c3bac1ce8e01f8e429e20154008a4569b
MedawalaRMWD <medawalarmwd@gmail.com>
2026-09-10 23:58:20 +0530

    Add JSX wrapper to AdminAllBookings

--- webtourguide-web/src/pages/bookings/AdminAllBookings.jsx
+++ webtourguide-web/src/pages/bookings/AdminAllBookings.jsx
@@ -4,4 +4,10 @@ import { getAllBookings } from "../../api/bookingApi";
 export default function AdminAllBookings() {
   const [bookings, setBookings] = useState([]);
   useEffect(() => { getAllBookings().then(setBookings); }, []);
+
+  return (
+    <div className="page">
+      <h1>All Bookings (Oversight)</h1>
+    </div>
+  );
 }
