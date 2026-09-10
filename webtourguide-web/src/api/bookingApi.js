c7c2b921b8d27fa298b5923a773a04d9320109f5
MedawalaRMWD <medawalarmwd@gmail.com>
2026-09-10 23:58:15 +0530

    Add getMyBookings API method

--- webtourguide-web/src/api/bookingApi.js
+++ webtourguide-web/src/api/bookingApi.js
@@ -1,3 +1,4 @@
 import axiosClient from "./axiosClient";
 
 export const createBooking = (data) => axiosClient.post("/bookings", data).then((r) => r.data);
+export const getMyBookings = () => axiosClient.get("/bookings/my").then((r) => r.data);
