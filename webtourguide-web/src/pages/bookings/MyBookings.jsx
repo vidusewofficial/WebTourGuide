4bbf050375d41efbcd2da0f5838c7094e8124a2c
MedawalaRMWD <medawalarmwd@gmail.com>
2026-09-10 23:58:18 +0530

    Add state and useEffect to MyBookings

--- webtourguide-web/src/pages/bookings/MyBookings.jsx
+++ webtourguide-web/src/pages/bookings/MyBookings.jsx
@@ -2,4 +2,8 @@ import { useEffect, useState } from "react";
 import { getMyBookings, cancelBooking, rescheduleBooking } from "../../api/bookingApi";
 
 export default function MyBookings() {
+  const [bookings, setBookings] = useState([]);
+
+  function refresh() { getMyBookings().then(setBookings); }
+  useEffect(refresh, []);
 }
