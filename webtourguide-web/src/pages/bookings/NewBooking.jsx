3c2f3f7e622bd1a4efce6a30b8707e658d2bb38b
MedawalaRMWD <medawalarmwd@gmail.com>
2026-09-10 23:58:16 +0530

    Create NewBooking component shell

---
+++ webtourguide-web/src/pages/bookings/NewBooking.jsx
@@ -0,0 +1,6 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { createBooking } from "../../api/bookingApi";
+
+export default function NewBooking() {
+}
