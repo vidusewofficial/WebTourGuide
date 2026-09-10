3a480fcda3874ccdbdb1bc6ac0240b63185009cf
MedawalaRMWD <medawalarmwd@gmail.com>
2026-09-10 23:58:16 +0530

    Add cancelBooking API method

--- webtourguide-web/src/api/bookingApi.js
+++ webtourguide-web/src/api/bookingApi.js
@@ -3,3 +3,4 @@ import axiosClient from "./axiosClient";
 export const createBooking = (data) => axiosClient.post("/bookings", data).then((r) => r.data);
 export const getMyBookings = () => axiosClient.get("/bookings/my").then((r) => r.data);
 export const getAllBookings = () => axiosClient.get("/bookings").then((r) => r.data);
+export const cancelBooking = (id) => axiosClient.patch(`/bookings/${id}/cancel`).then((r) => r.data);
