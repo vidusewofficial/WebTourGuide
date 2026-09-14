f9b9d1fc9bc49c787f91bc29ad2241392ae3e140
AponsuLASK <aponsulask@gmail.com>
2026-09-14 12:51:09 +0530

    Add doc comment to SupportTicketRepository

--- webtourguide-api/src/main/java/com/webtourguide/support/SupportTicketRepository.java
+++ webtourguide-api/src/main/java/com/webtourguide/support/SupportTicketRepository.java
@@ -3,6 +3,7 @@ package com.webtourguide.support;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
+/** Data access for support tickets, with owner and status lookups. */
 public interface SupportTicketRepository extends JpaRepository<SupportTicket, Long> {
     List<SupportTicket> findByRaisedById(Long touristId);
     List<SupportTicket> findByStatus(TicketStatus status);
