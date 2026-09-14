d54ee54815027ab24cb2ebf8a2890b00bfe25cc9
AponsuLASK <aponsulask@gmail.com>
2026-09-14 12:51:12 +0530

    Add doc comment to TicketStatusUpdateRequest

--- webtourguide-api/src/main/java/com/webtourguide/support/dto/TicketStatusUpdateRequest.java
+++ webtourguide-api/src/main/java/com/webtourguide/support/dto/TicketStatusUpdateRequest.java
@@ -4,6 +4,7 @@ import com.webtourguide.support.TicketStatus;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
+/** Request body staff or admin send to move a ticket to a new status. */
 @Data
 public class TicketStatusUpdateRequest {
     @NotNull private TicketStatus status;
