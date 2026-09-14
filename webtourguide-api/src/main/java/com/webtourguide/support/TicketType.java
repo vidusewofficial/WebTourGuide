20f38df107c8cccd1074ef40cee69d91f87429cc
AponsuLASK <aponsulask@gmail.com>
2026-09-14 12:51:07 +0530

    Add doc comment to TicketType enum

--- webtourguide-api/src/main/java/com/webtourguide/support/TicketType.java
+++ webtourguide-api/src/main/java/com/webtourguide/support/TicketType.java
@@ -1,3 +1,4 @@
 package com.webtourguide.support;
 
+/** Kind of request a tourist can raise with support. */
 public enum TicketType { INQUIRY, COMPLAINT, CANCELLATION_REQUEST, RESCHEDULE_REQUEST }
